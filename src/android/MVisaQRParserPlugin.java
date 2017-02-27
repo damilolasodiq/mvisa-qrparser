08c89182989f38143b5ae554699c0380fe8b7cdb
P7069 <sodiq.fagbola>
2017-02-27 11:36:12 +0100

    Added all getters from the QRParser jar to the plugin

--- src/android/MVisaQRParserPlugin.java
+++ src/android/MVisaQRParserPlugin.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import com.visa.mvisa.tlvparser.*;
 
 public class MVisaQRParserPlugin extends CordovaPlugin {
@@ -21,12 +22,8 @@ public class MVisaQRParserPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         Log.v(TAG, "Executing action: " + action);
-        switch (action) {
-            case PARSE_QR_DATA:
-              parseQrData(callbackContext);
-              break;
-            default:
-                return false;
+        if(action.equalsIgnoreCase(PARSE_QR_DATA)){
+          parseQrData(args,callbackContext);
         }
         return true;
     }
@@ -36,17 +33,68 @@ public class MVisaQRParserPlugin extends CordovaPlugin {
         try {
             String qrDataString = args.getString(0);
             QrCodeParser qrCodeParser = new QrCodeParser();
-            QrCodeParserResponse qrCodeParserResponse = qrCodeParser.parseQrData(qrDataString);
+            QrCodeParserResponse qrCodeParserResponse = qrCodeParser.parseQrCode(qrDataString);
+
             if(qrCodeParserResponse!=null){
-              if(qrCodeParserResponse.getQrCodeError()!=null && !qrCodeParserResponse.getQrCodeError().isEmpty()){
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, qrCodeParserResponse.getQrCodeError()));
+              if(qrCodeParserResponse.getErrorCodesList()!=null && !qrCodeParserResponse.getErrorCodesList().isEmpty()){
+                JSONArray jsonArray = new JSONArray();
+                for (Integer errorCode:qrCodeParserResponse.getErrorCodesList()) {
+                  jsonArray.put(errorCode);
+                }
+                callbackContext.error(jsonArray.toString());
               }else{
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, qrCodeParserResponse.getQrCodeData()));
+                JSONObject reponseObject = new JSONObject();
+                QrCodeData qrCodeData= qrCodeParserResponse.getQrCodeData();
+                reponseObject.put("payloadFormatIndicator", qrCodeData.getPayloadFormatIndicator());
+                reponseObject.put("pointOfInitiation", qrCodeData.getPointOfInitiation());
+                reponseObject.put("merchantId", qrCodeData.getMerchantId());
+                reponseObject.put("masterCardPan1", qrCodeData.getMasterCardPan1());
+                reponseObject.put("masterCardPan2", qrCodeData.getMasterCardPan2());
+                reponseObject.put("npciid1", qrCodeData.getNpciid1());
+                reponseObject.put("npciid2", qrCodeData.getNpciid2());
+                reponseObject.put("mcc", qrCodeData.getMcc());
+                reponseObject.put("currencyCode", qrCodeData.getCurrencyCode());
+                reponseObject.put("transactionAmount", qrCodeData.getTransactionAmount());
+                reponseObject.put("tipIndicatorValue", qrCodeData.getTipIndicatorValue());
+                reponseObject.put("tipAmount", qrCodeData.getTipAmount());
+                reponseObject.put("tipPercentage", qrCodeData.getTipPercentage());
+                reponseObject.put("countryCode", qrCodeData.getCountryCode());
+                reponseObject.put("merchantName", qrCodeData.getMerchantName());
+                reponseObject.put("cityName", qrCodeData.getCityName());
+                reponseObject.put("postalCode", qrCodeData.getPostalCode());
+                reponseObject.put("billId", qrCodeData.getBillId());
+                reponseObject.put("mobileNumber", qrCodeData.getMobileNumber());
+                reponseObject.put("storeId", qrCodeData.getStoreId());
+                reponseObject.put("loyaltyNumber", qrCodeData.getLoyaltyNumber());
+                reponseObject.put("referenceId", qrCodeData.getReferenceId());
+                reponseObject.put("consumerId", qrCodeData.getConsumerId());
+                reponseObject.put("terminalId", qrCodeData.getTerminalId());
+                reponseObject.put("purpose", qrCodeData.getPurpose());
+                reponseObject.put("additionalConsumerDataRequest", qrCodeData.getAdditionalConsumerDataRequest());
+                reponseObject.put("addDataMasterCard1", qrCodeData.getAddDataMasterCard1());
+                reponseObject.put("addDataMasterCard2", qrCodeData.getAddDataMasterCard2());
+                reponseObject.put("addDataNpci1", qrCodeData.getAddDataNpci1());
+                reponseObject.put("addDataNpci2", qrCodeData.getAddDataNpci2());
+                reponseObject.put("crc", qrCodeData.getCrc());
+                reponseObject.put("isBillIdMandatory", qrCodeData.isBillIdMandatory());
+                reponseObject.put("isMobileNoMandatory", qrCodeData.isMobileNoMandatory());
+                reponseObject.put("isStoreIdMandatory", qrCodeData.isStoreIdMandatory());
+                reponseObject.put("isLoyaltyNoMandatory", qrCodeData.isLoyaltyNoMandatory());
+                reponseObject.put("isReferenceIdMandatory", qrCodeData.isReferenceIdMandatory());
+                reponseObject.put("isConsumerIdMandatory", qrCodeData.isConsumerIdMandatory());
+                reponseObject.put("isTerminalIdMandatory", qrCodeData.isTerminalIdMandatory());
+                reponseObject.put("isPurposeMandatory", qrCodeData.isPurposeMandatory());
+                reponseObject.put("isAdditonalConsumerDataRequestMandatory", qrCodeData.isAdditonalConsumerDataRequestMandatory());
+                reponseObject.put("isAddDataMasterCard1Mandatory", qrCodeData.isAddDataMasterCard1Mandatory());
+                reponseObject.put("isAddDataMasterCard2Mandatory", qrCodeData.isAddDataMasterCard2Mandatory());
+                reponseObject.put("isAddDataNpci1Mandatory", qrCodeData.isAddDataNpci1Mandatory());
+                reponseObject.put("isAddDataNpci2Mandatory", qrCodeData.isAddDataNpci2Mandatory());
+                callbackContext.success(reponseObject);
               }
             }
         } catch (Exception e) {
             Log.e(TAG,"Error while reading scanner qr code",e);
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, e.getMessage()));
+            callbackContext.error(e.getMessage());
         }
         return;
     }
